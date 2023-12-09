bdfcd15f47ecc2932b2c1ba1edffaf9d3d3ab0ae
オタコン悪 <otaconeviloficial@gmail.com>
2023-12-09 16:59:16 -0300

    Delete Restaurar la Imagen de Windows.bat

--- Restaurar la Imagen de Windows.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-chcp 65001 > nul
-color 30
-title https://github.com/StarkTechRepo
-
-echo Restaurando la Imagen de Windows ante Cambios Inesperados...
-DISM /Online /Cleanup-Image /RestoreHealth
