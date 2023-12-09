5034d0411fcd11d9eec4814f5d8d2716ad99b9b2
オタコン悪 <otaconeviloficial@gmail.com>
2023-12-09 16:58:37 -0300

    Delete Kaspersky Rescue Disk.bat

--- Kaspersky Rescue Disk.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-chcp 65001 > nul
-color 30
-title https://github.com/StarkTechRepo
-
-echo Descargando Kaspersky Rescue Disk...
-powershell -Command "Invoke-WebRequest -Uri 'https://rescuedisk.s.kaspersky-labs.com/latest/krd.iso' -OutFile 'krd.iso'"
